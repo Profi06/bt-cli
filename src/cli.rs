735da5048ba3f8fc8cbfb9e9350353623edeae61
Profi06 <45404278+Profi06@users.noreply.github.com>
2024-09-09 22:45:39 +0200

    Changed cli args (full_match -> no-partial), added args partial, regex, no-regex, color, no-color, address with last three args not implemented yet

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,10 @@
-use clap::{command, value_parser, Arg, ArgAction, Command};
+use clap::{command, value_parser, Arg, ArgAction, ArgGroup, Command};
 
 pub fn build_cli() -> Command {
     let name_arg = Arg::new("filter")
         .index(1)
         .required(true)
         .help("Device filter.");
-    let full_match_arg = Arg::new("full_match")
-        .short('f').long("full_match")
-        .action(ArgAction::SetTrue)
-        .help("If set the device name must be an exact match");
     let timeout_arg = Arg::new("timeout")
         .short('t').long("timeout")
         .value_parser(value_parser!(u32))
@@ -16,8 +12,60 @@ pub fn build_cli() -> Command {
         .long_help("Timeout for scanning and pairing attempts in seconds\n\
             Default can be controlled with environment variable BT_TIMEOUT");
 
+    let color_arg = Arg::new("color")
+        .short('c').long("color")
+        .action(ArgAction::SetTrue)
+        .conflicts_with("no-color")
+        .help("Uses ANSI escape sequences to print with text formatting and \
+            color if used");
+    let no_color_arg = Arg::new("no-color")
+        .short('C').long("no-color")
+        .action(ArgAction::SetTrue)
+        .conflicts_with("color")
+        .help("Disallow usage of ANSI escape sequences");
+    let color_arg_group = ArgGroup::new("color group")
+        .args(["color", "no-color"]);
+
+    let partial_arg = Arg::new("partial")
+        .short('p').long("partial")
+        .action(ArgAction::SetTrue)
+        .help("If set the filter must only match part of the device name.");
+    let no_partial_arg = Arg::new("no-partial")
+        .short('P').long("no-partial")
+        .action(ArgAction::SetTrue)
+        .help("If set the filter must match the full device name.");
+    let partial_arg_group = ArgGroup::new("partial group")
+        .args(["partial", "no-partial"]);
+
+    let regex_arg = Arg::new("regex")
+        .short('r').long("regex")
+        .action(ArgAction::SetTrue)
+        .help("If set the filter is interpreted as a regex pattern.");
+    let no_regex_arg = Arg::new("no-regex")
+        .short('R').long("no-regex")
+        .action(ArgAction::SetTrue)
+        .help("If set the filter is applied literally");
+    let regex_arg_group = ArgGroup::new("regex group")
+        .args(["regex", "no-regex"]);
+
+    let address_arg = Arg::new("address")
+        .short('a').long("address")
+        .action(ArgAction::SetTrue)
+        .help("Filter is matched against the address instead of name.");
+    let fields_arg = Arg::new("filter-fields")
+        .short('f').long("filter-fields")
+        .action(ArgAction::SetTrue);
+    let filter_arg_group = ArgGroup::new("filter group")
+        .args(["address", "filter-fields"]);
+
+
     command!()
         .propagate_version(true)
+        .args([
+            color_arg,
+            no_color_arg,
+        ])
+        .group(color_arg_group)
         .subcommands([
             Command::new("list")
                 .visible_alias("ls")
@@ -47,21 +95,49 @@ pub fn build_cli() -> Command {
                 .before_help("Connect to a bluetooth device")
                 .args([
                     name_arg.clone(), 
-                    full_match_arg.clone()
+                    partial_arg.clone(),
+                    no_partial_arg.clone(),
+                    regex_arg.clone(),
+                    no_regex_arg.clone(),
+                    address_arg.clone(),
+                    fields_arg.clone(),
+                    timeout_arg.clone(),
+                ]).groups([
+                    partial_arg_group.clone(),
+                    regex_arg_group.clone(),
+                    filter_arg_group.clone(),
                 ]),
             Command::new("disconnect")
                 .visible_alias("dc")
                 .before_help("Disconnect from a bluetooth device")
                 .args([
                     name_arg.clone(), 
-                    full_match_arg.clone()
+                    partial_arg.clone(),
+                    no_partial_arg.clone(),
+                    regex_arg.clone(),
+                    no_regex_arg.clone(),
+                    address_arg.clone(),
+                    fields_arg.clone(),
+                ]).groups([
+                    partial_arg_group.clone(),
+                    regex_arg_group.clone(),
+                    filter_arg_group.clone(),
                 ]),
             Command::new("info")
                 .visible_alias("i")
                 .before_help("Get detailed information about a bluetooth device")
                 .args([
                     name_arg.clone(), 
-                    full_match_arg.clone()
+                    partial_arg.clone(),
+                    no_partial_arg.clone(),
+                    regex_arg.clone(),
+                    no_regex_arg.clone(),
+                    address_arg.clone(),
+                    fields_arg.clone(),
+                ]).groups([
+                    partial_arg_group.clone(),
+                    regex_arg_group.clone(),
+                    filter_arg_group.clone(),
                 ]),
             Command::new("pair")
                 .visible_alias("p")
@@ -69,8 +145,17 @@ pub fn build_cli() -> Command {
                 .before_help("Pair with a bluetooth device")
                 .args([
                     name_arg.clone(), 
-                    full_match_arg.clone(), 
-                    timeout_arg.clone()
+                    partial_arg.clone(),
+                    no_partial_arg.clone(),
+                    regex_arg.clone(),
+                    no_regex_arg.clone(),
+                    address_arg.clone(),
+                    fields_arg.clone(),
+                    timeout_arg.clone(),
+                ]).groups([
+                    partial_arg_group.clone(),
+                    regex_arg_group.clone(),
+                    filter_arg_group.clone(),
                 ]),
             Command::new("unpair")
                 .visible_alias("up")
@@ -78,7 +163,16 @@ pub fn build_cli() -> Command {
                 .before_help("Remove a bluetooth device")
                 .args([
                     name_arg.clone(), 
-                    full_match_arg.clone()
+                    partial_arg.clone(),
+                    no_partial_arg.clone(),
+                    regex_arg.clone(),
+                    no_regex_arg.clone(),
+                    address_arg.clone(),
+                    fields_arg.clone(),
+                ]).groups([
+                    partial_arg_group.clone(),
+                    regex_arg_group.clone(),
+                    filter_arg_group.clone(),
                 ]),
-        ])
+    ])
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ mod cli;
 
 use std::{env, io::{stdout, IsTerminal}};
 use bluetooth::*;
+use clap::ArgMatches;
 
 fn main() {
-
     // Initialize empty device list and set values
     let mut devicelist = DeviceList::new();
     let stdout_is_terminal = stdout().lock().is_terminal();
@@ -20,53 +20,54 @@ fn main() {
             let long_output = sub_matches.get_flag("long_output");
             let linewise = sub_matches.get_flag("linewise");
             let all = sub_matches.get_flag("all");
-            let timeout = get_timeout(&sub_matches.get_one("timeout").copied(), Some(30));
+            let timeout = get_timeout(
+                &sub_matches.get_one("timeout").copied(), 30);
 
             devicelist.fill(if all { timeout } else { None });
             devicelist.print(linewise, long_output);
         }
         Some(("connect", sub_matches)) => {
-            let name = sub_matches.get_one::<String>("filter").expect("name is required");
-            let full_match = sub_matches.get_flag("full_match");
+            let filter = sub_matches.get_one::<String>("filter")
+                .expect("filter is required");
             let count = devicelist
                 .fill(None)
-                .filtered_name(name, get_behaviour(full_match))
+                .filtered_name(filter, get_behaviour(sub_matches))
                 .connect_all();
             println!("Connected {} devices.", count);
         }
         Some(("disconnect", sub_matches)) => {
-            let name = sub_matches.get_one::<String>("filter").expect("name is required");
-            let full_match = sub_matches.get_flag("full_match");
+            let filter = sub_matches.get_one::<String>("filter")
+                .expect("filter is required");
             let count = devicelist
                 .fill(None)
-                .filtered_name(name, get_behaviour(full_match))
+                .filtered_name(filter, get_behaviour(sub_matches))
                 .disconnect_all();
             println!("Disconnected {} devices.", count);
         }
         Some(("info", sub_matches)) => {
-            let name = sub_matches.get_one::<String>("filter").expect("name is required");
-            let full_match = sub_matches.get_flag("full_match");
+            let filter = sub_matches.get_one::<String>("filter")
+                .expect("filter is required");
             devicelist.fill(None)
-                .filtered_name(name, get_behaviour(full_match)) 
+                .filtered_name(filter, get_behaviour(sub_matches)) 
                 .print_info_all();
         }
         Some(("pair", sub_matches)) => {
-            let name = sub_matches.get_one::<String>("filter").expect("name is required");
-            let full_match = sub_matches.get_flag("full_match");
-            let timeout = get_timeout(&sub_matches.get_one("timeout").copied(), Some(5));
-            println!("Scanning for nearby pairable devices...");
+            let filter = sub_matches.get_one::<String>("filter")
+                .expect("filter is required");
+            let timeout = get_timeout(
+                &sub_matches.get_one("timeout").copied(), 5);
             let count = devicelist
                 .fill(timeout)
-                .filtered_name(name, get_behaviour(full_match))
+                .filtered_name(filter, get_behaviour(sub_matches))
                 .pair_all();
             println!("Paired {} devices.", count);
         }
         Some(("unpair", sub_matches)) => {
-            let name = sub_matches.get_one::<String>("filter").expect("name is required");
-            let full_match = sub_matches.get_flag("full_match");
+            let filter = sub_matches.get_one::<String>("filter")
+                .expect("filter is required");
             let count = devicelist
                 .fill(None)
-                .filtered_name(name, get_behaviour(full_match))
+                .filtered_name(filter, get_behaviour(sub_matches))
                 .unpair_all();
             println!("Unpaired {} devices.", count);
         },
@@ -78,17 +79,29 @@ fn main() {
     }
 }
 
-fn get_timeout(param: &Option<u32>, default: Option<u32>) -> Option<u32> {
+fn get_timeout(param: &Option<u32>, default: u32) -> Option<u32> {
     param.or_else(|| {match env::var("BT_TIMEOUT") {
-        Ok(var) => var.trim().parse().ok().or(default),
-        Err(_) => default,
+        Ok(var) => var.trim().parse().ok().or(Some(default)),
+        Err(_) => Some(default),
     }})
 }
 
-fn get_behaviour(full_match: bool) -> FilterBehaviour {
-    if full_match {
-        FilterBehaviour::Full
+fn get_behaviour(matches: &ArgMatches) -> FilterBehaviour {
+    let partial = *matches.get_one::<bool>("partial").unwrap_or(&true) 
+        && !matches.get_one::<bool>("no-partial").unwrap_or(&false);
+    let regex = *matches.get_one::<bool>("regex").unwrap_or(&false) 
+        && !matches.get_one::<bool>("no-regex").unwrap_or(&true);
+    if partial {
+        if regex {
+            FilterBehaviour::ContainsRegex
+        } else {
+            FilterBehaviour::Contains
+        }
     } else {
-        FilterBehaviour::Contains
+        if regex {
+            FilterBehaviour::FullRegex
+        } else {
+            FilterBehaviour::Full
+        }
     }
 }
